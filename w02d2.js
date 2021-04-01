0f7818b96f54cb3c6aa935d99501ab951ba50760
kleddy17 <kleddy17@gmail.com>
2021-04-01 15:10:24 -0400

    math-methods

--- w02d2.js
+++ w02d2.js
@@ -1,11 +1,33 @@
 // Math Methods
 
-let cartTotal = 16.67
-let roundedUpCart = Math.ceil(cartTotal);
-let donation = (roundedUpCart-cartTotal).toFixed(2)
-console.log(donation)
+// let cartTotal = 16.67
+// let roundedUpCart = Math.ceil(cartTotal);
+// let donation = (roundedUpCart-cartTotal).toFixed(2)
+// console.log(donation)
 
 //Math.round = rounds to the nearest whole number
 
 //Math.ceil rounds up to the nearest whole number
+
+
+// An array is a list of elements all items go between square brackets
+
+//an example of an empty array
+const arr = [];
+console.log(arr)
+
+//an example of an array with values
+const favFoods = ["pizza", "tacos", "icecream"]
+console.log(favFoods)
+
+
+// Type Checking
+const num = 0;
+const str = "strings are cool";
+const bool = true;
+console.log(typeof num);
+console.log(typeof str);
+console.log(bool);
+
+// Objects give us methods so that we can perform functionality on the data strucutre
 
